72367d634dd31b4194f1f1a67631eec1644c2d78
Dagim <dagimgetaW27@gmail.com>
2024-06-14 20:49:18 +0300

    check if it is valid paretheses

---
+++ leetcode/valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        for char in s:
+            if char in ['(', '{', '[']:
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                current_char = stack.pop()
+                if current_char == '(':
+                    if char != ')':
+                        return False
+                if current_char == '{':
+                    if char != '}':
+                        return False
+                if current_char == '[':
+                    if char != ']':
+                        return False
+        if stack:
+            return False
+        return True
