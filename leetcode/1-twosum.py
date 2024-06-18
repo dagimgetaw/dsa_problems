b579edd29d1e39c2a33a95d0ca0472d23e8c2870
Dagim <165191539+dagimgetaw@users.noreply.github.com>
2024-06-18 14:05:40 +0300

    Delete leetcode/1-twosum.py

--- leetcode/1-twosum.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        length = len(nums)
-        for i in range(length - 1):
-            for j in range(i+1, length):
-                if nums[i] + nums[j] == target:
-                    return(i, j)
