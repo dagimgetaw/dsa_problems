fad4ea3eaf4521c876ca15426c33e43c96da9e09
Dagim <165191539+dagimgetaw@users.noreply.github.com>
2024-06-18 14:05:53 +0300

    Delete leetcode/28-first-occurrence-in-a-string.py

--- leetcode/28-first-occurrence-in-a-string.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def strStr(self, haystack, needle):
-        index = haystack.find(needle)
-        if index >= 0:
-            return(index)
-        else:
-            return(-1)
