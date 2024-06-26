32875f2a7399721c6f8053f0914aa3b715214fd6
Dagim <dagimgetaW27@gmail.com>
2024-06-26 22:29:06 +0300

    number of 1 bits

---
+++ leetcode/191-number-of-1-bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            count += n & 1
+            n >>= 1
+            
+        return count
+        
