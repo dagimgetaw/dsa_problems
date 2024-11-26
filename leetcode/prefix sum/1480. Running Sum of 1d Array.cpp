0f5504eac2e6d22b3ea7a892a0a044f7da835faf
Dagim <dagimgetaW27@gmail.com>
2024-11-26 10:02:32 +0300

    1480. Running Sum of 1d Array

---
+++ leetcode/prefix sum/1480. Running Sum of 1d Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int> prefixsum(n);
+
+        prefixsum[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            prefixsum[i] = prefixsum[i-1] + nums[i];
+        }
+
+        return prefixsum;
+        
+    }
+};
